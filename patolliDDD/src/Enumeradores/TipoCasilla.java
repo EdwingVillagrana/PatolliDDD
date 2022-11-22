3c3f56d390e11eb218b9fd339cbaedd12de882f2
MiguelAngelDeLaRosa <101830592+MiguelAngelDeLaRosa@users.noreply.github.com>
2022-11-22 15:03:59 -0600

    Add files via upload
    Enumeradores

---
+++ patolliDDD/src/Enumeradores/TipoCasilla.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Enumeradores;
+
+/**
+ *
+ * @author Miguel
+ */
+public enum TipoCasilla {
+    
+    CENTRAL,
+    CUADRADA,
+    CUADRADA_CON_CASTIGO,
+    REDONDA,
+    TRIANGULAR
+}
