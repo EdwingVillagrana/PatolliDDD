976b35e84345ba296de3a3e3fe290b1c2e40a6fa
Edwing06 <105477451+Edwing06@users.noreply.github.com>
2022-11-24 22:45:14 -0700

    Creación del paquete aplicación
    Se creó el paquete aplicación y la clase Aplicación con el esqueleto de los métodos a utilizarse, basándonos en los casos de uso del sistema

---
+++ patolliDDD/src/aplicacion/Aplicacion.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package aplicacion;
+
+/**
+ *
+ * @author edw_v
+ */
+public class Aplicacion {
+
+    public void crearPartida(){
+        
+    }
+    
+    public void unirseAPartida(){
+        
+    }
+    
+    public void salirDePartida(){
+        
+    }
+    
+    public void iniciarPartida(){
+        
+    }
+    
+    public void ejercerTurno(){
+        
+    }
+    
+}
